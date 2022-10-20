ce864c0e7e04871a3e779c3063e7d7ac865d11c8
Francisco Florio <fflorio@algeiba.com>
2022-10-20 15:50:41 -0300

    Fetch conaxion

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import List from './Components/List';
 import Form from './Components/Form';
-import {Sub} from './types.d';
-
-const INITIAL_STATE=[
-    {
-      nick: 'locostat2',
-      subMonth: 3,
-      avatar: 'https://i.pravatar.cc/150?u=locostat2',
-      description: 'a este tipo le tenemos que cambiar el nick'
-    },
-    {
-      nick: 'Anonymous',
-      subMonth: 4,
-      avatar: 'https://i.pravatar.cc/150?u=anonymous'
-    }
-  ];
+import {Sub,SubsResponseFromApi} from './types.d';
+import axios from 'axios';
+import { SubstitutionType } from 'typescript';
 
 
 interface AppState{
@@ -27,21 +15,49 @@ interface AppState{
 function App() {
 
   const [subs,setSubs] = useState<AppState["sub"]>([]);
-  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>();
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0);
   const divRef = useRef<HTMLDivElement>(null);
 
   useEffect( () => {
-    setSubs(INITIAL_STATE)
+    const fetchSubs = ()=>{
+      return axios
+      .get<SubsResponseFromApi>('http://localhost:300/subs')
+      .then(response => response.data)
+    }
+
+  const mapFromApiToSubs = ( apiResponse: SubsResponseFromApi):
+    Array<Sub> => {
+      return apiResponse.map(subFromApi => {
+        const{
+          months: subMonth,
+          profileUrl:avatar,
+          nick,
+          description,
+        } = subFromApi
+        return {
+          nick,
+          subMonth,
+          description,
+          avatar
+        }
+      })
+    }
+
+  fetchSubs()
+     .then(mapFromApiToSubs)
+    .then(setSubs)
   }, [])
 
   const handleNewSubs = (newSub: Sub): void =>{
     setSubs(subs => [...subs,newSub])
+    setNewSubsNumber(n => n + 1)
   }
   
   return (
     <div className="App" ref= {divRef}>
       <h1>Subscriptores!</h1>
       <List subs={subs} />
+      New sub: {newSubsNumber}
       <Form onNewSub={handleNewSubs}/>
     </div>
   );
